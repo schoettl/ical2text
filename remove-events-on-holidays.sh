cc588affe33bf10bba6d8831764ab5420bb4216d
Jakob Schöttl <jschoett@gmail.com>
2017-01-04 19:27:20 +0100

    Add scripts to process Urlaubsrechnung

---
+++ remove-events-on-holidays.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Remove all events starting on holidays.
+
+printUsage() {
+    cat <<EOF
+usage: $PROGNAME <icalendar-ics-file>
+EOF
+}
+
+readonly PROGNAME=$(basename "$0")
+
+# $1: error message
+exitWithError() {
+    echo "$1" >&2
+    exit 1
+}
+
+main() {
+    (( $# == 1 )) || exitWithError "$(printUsage)"
+    declare holidaysFile=$1
+    declare holidaysText
+    holidaysText=$(mktemp)
+
+    [[ -r $holidaysFile ]] || exitWithError "error: cannot open file for read: $holidaysFile"
+    # this error cannot be catched below because of pipe and/or <
+
+    # delimiter: "T" in "2017-01-04T19:30"
+    ical2text < "$holidaysFile" | cut -dT -f1 | sed 's/^/^/' > "$holidaysText"
+    # cat "$holidaysText"
+
+    # print those that are not in the holidays pattern file
+    grep -vf "$holidaysText"
+}
+
+main "$@"
---
+++ remove-events-on-weekends.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Remove events on weekends. Only the start time is considered!
+# In other words: grep for events that take place Mo - Fr.
+
+# $1: start date in ISO format
+isWeekend() {
+    declare dayOfWeek
+    dayOfWeek=$(date -d "$1" +%u)
+    (( dayOfWeek >= 6 )) # is weekend
+}
+
+main() {
+    declare start rest
+    while read -r start rest; do
+        if ! isWeekend "$start"; then
+            echo "$start $rest"
+        fi
+    done
+}
+
+main
