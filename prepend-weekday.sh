9e8861511545f33bf515b5bb5d60e48e00523f55
Jakob Schöttl <jschoett@gmail.com>
2018-01-25 09:59:54 +0100

    Add useful option to script

--- prepend-weekday.sh
+++ prepend-weekday.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Prepend a column with the weekday number: 1 (Mon) to 7 (Sun)
+# -h -> human readable (Mon to Sun)
 
 main() {
     declare weekDay start rest
     while read -r start rest; do
-        weekDay=$(date -d "$start" +%u)
+        if [[ $1 == -h ]]; then
+            weekDay=$(LC_DATE=C date -d "$start" +%a)
+        else
+            weekDay=$(date -d "$start" +%u)
+            # %w would be 0-6 (Sun to Sat)
+        fi
         echo "$weekDay $start $rest"
     done
 }
 
-main
+main "$@"
